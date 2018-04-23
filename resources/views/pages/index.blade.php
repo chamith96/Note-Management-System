0db52191eae13ef45a14ab06d523be1af7a7f915
chamith96 <chamithsandagiri@gmail.com>
2018-04-23 11:20:52 +0530

    add images

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 
   @section('content')
     <div class="page-header" align="center">
-  <img src="https://i.kinja-img.com/gawker-media/image/upload/s--ywN32ag7--/c_scale,fl_progressive,q_80,w_800/18m5vfjl1jqjmjpg.jpg" alt="note">
+  <img src="{{ asset('images/img1.jpg') }}" alt="note">
   <h3><b>Manage your notes Secure with us.....<br>Just log in your cloud profile any time if don't have pen and paper</b></h3>
 </div>
   @endsection
